7f86659b98a00238ee4b84bc97422dfef9a25dd9
triopscmd <mtrabajoraul@gmail.com>
2025-08-01 14:03:20 +0200

    feat(CFG-4): Implement tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+```javascript
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
